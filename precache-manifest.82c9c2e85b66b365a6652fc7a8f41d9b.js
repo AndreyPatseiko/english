61674c55c1d8b39059cb54474a95deb716d67e38
AndreyPatseiko <a.patseiko@gmail.com>
2019-06-10 22:07:15 +0300

    Updates

---
+++ precache-manifest.82c9c2e85b66b365a6652fc7a8f41d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4dc2e0b4b5b5950025c2d2bd7a4ee4",
+    "url": "/AndreyPatseiko/english/index.html"
+  },
+  {
+    "revision": "e65455113016ae6a3635",
+    "url": "/AndreyPatseiko/english/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/AndreyPatseiko/english/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e65455113016ae6a3635",
+    "url": "/AndreyPatseiko/english/static/js/main.e097685a.chunk.js"
+  },
+  {
+    "revision": "b94ff86f8587da5e2316",
+    "url": "/AndreyPatseiko/english/static/js/runtime~main.e8945e77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AndreyPatseiko/english/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AndreyPatseiko/english.git/precache-manifest.c85b69cfde566158ef71bb238e6e93bd.js"
+  "/AndreyPatseiko/english/precache-manifest.82c9c2e85b66b365a6652fc7a8f41d9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AndreyPatseiko/english.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AndreyPatseiko/english/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
